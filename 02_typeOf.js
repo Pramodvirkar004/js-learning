1c2b4c3e5d55b64a901d068a0c7e61d49b92cd42
Pramodvirkar004 <pramodvirkar004@gmail.com>
2024-03-09 09:19:23 -0800

    Variable,data type, type of operator assignment
    Variable ,data type, type of operator assignment

---
+++ 02_typeOf.js
@@ -0,0 +1,47 @@
+console.log("********Step 1**********");
+console.log("==============Before Initizalation=== ");
+var Bankname; 
+console.log("Value of Bankname:", Bankname);
+var typeOfBankname = typeof Bankname;
+console.log("Type of Bankname:", typeOfBankname);
+console.log("==============Aftre Initizalation=== ");
+Bankname = "SBI Bank"
+console.log("Value of Bankname:", Bankname);
+var result = typeof Bankname;
+console.log("Type of Bankname",result);
+
+console.log("***********Step 2************");
+var marks="90%";
+console.log("Value of marks:", marks);
+var typeofmarks = typeof marks;
+console.log("Type of marks:", typeofmarks);
+
+console.log("***********Step 3*************");
+console.log("==============Before Initizalation=== ");
+var isWorking; 
+console.log("Value of isWorking:", isWorking);
+var typeOfisWorking = typeof isWorking;
+console.log("Type of isWorking:", typeOfisWorking);
+console.log("==============Aftre Initizalation=== ");
+isWorking = true
+console.log("Value of isWorking:", isWorking);
+var result = typeof isWorking;
+console.log("Type of isWorking",result);
+
+console.log("***********Step 4*************");
+
+var totalCount="Hundred and Seven";
+console.log("Value of totalCount:", totalCount);
+var totalCount = typeof totalCount;
+console.log("Type of totalCount:", typeof totalCount);
+
+console.log("==============Aftre Update======== ");
+
+var totalCount= 107;
+console.log("Value of totalCount:", totalCount);
+var totalCount = typeof totalCount;
+console.log("Type of totalCount:", typeof totalCount);
+
+
+
+
