5600d7f05e7d157ab8a051a7679d85c0f6164d75
Pramodvirkar004 <pramodvirkar004@gmail.com>
2024-03-09 06:06:18 -0800

    Variable and Data types Assignment Session 22
    Variable and Data types Assignment Session 22

---
+++ 01_swapVariableAssignment.js
@@ -0,0 +1,27 @@
+console.log("============Assignment 1================");
+console.log("WAP to swap variable values using third variable");
+var Sweety = "Sweety"; 
+var Cutie = "Cutie"; 
+console.log("======== Before Swap ==========");
+console.log("Sweety:", Sweety, " Cutie:", Cutie);
+
+console.log("======== After Swap ==========");
+var temp = Sweety;
+Sweety = Cutie;
+Cutie = temp;
+console.log("Sweety:", Sweety, " Cutie:", Cutie);
+
+console.log("============Assignment 2================");
+
+var num1 = 100; 
+var num2 = 200; 
+var num3 = 300;
+console.log("======== Before Swap ==========");
+console.log("num1:", num1, " num2:", num2,"num3:",num3);
+
+console.log("======== After Swap ==========");
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("num1:", num1, " num2:", num2,"num3:",num3);
---
+++ 01_variableAssign.js
@@ -0,0 +1,28 @@
+console.log("===========Assignment 01============");
+console.log("===========Step 1============");
+var myFullName;
+console.log("My Full name value befoer Initialization:", myFullName);
+myFullName="Pramod Virkar"
+console.log("My Full name value after Initialization:", myFullName);
+myFullName="Pramod Maruti Virkar"
+console.log("My Full name value after updating:", myFullName);
+console.log("===========Step 2============");
+var mylove="I love only JS";
+console.log("My love is:", mylove);
+console.log("===========Step 3============");
+var firstName="Pramod "
+console.log("My first name is:",firstName);
+var lastName="Virkar"
+console.log("My last name is:",lastName);
+var Age="23"
+console.log("Age is:",Age);
+var address="Satara"
+console.log("Address is:",address);
+var Pincode="415001"
+console.log("Pincode is:",Pincode);
+var Gender="Male"
+console.log("Gender is:",Gender);
+var isMarried="True"
+console.log("isMarried:",isMarried);
+
+
